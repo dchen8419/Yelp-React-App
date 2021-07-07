8b98b9abeffe70360cf1758e19dccdf431448a98
David Chen <DavidChen@Davids-iMac.local>
2021-07-07 11:18:23 -0400

    fixed phone number

--- src/Search/SearchResults/SearchResult/SearchResult.js
+++ src/Search/SearchResults/SearchResult/SearchResult.js
@@ -4,9 +4,20 @@ import { BusinessRating } from '../../../BusinessRating/BusinessRating';
 
 export function SearchResult (props) {
     const b = props.business
+    let phone = b.phone
     if(!b) {
         return <div></div>
     }
+
+    function formatPhoneNumber(phone) {
+        var cleaned = ('' + phone).replace(/\D/g, '');
+        var match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/);
+        if (match) {
+          var intlCode = (match[1] ? '+1 ' : '');
+          return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('');
+        }
+        return null;
+      }
 
     const tags = b.categories.map(catagory => (<span className={`tag ${styles['business-tag']}`} key={b.id + catagory.title}>{catagory.title}</span>))
     const addressLines = b.location.display_address.map(addressLine => <p key={b.id + addressLine}>{addressLine}</p>)   
@@ -20,7 +31,7 @@ export function SearchResult (props) {
                 <p>{b.price} {tags}</p>
             </div>
             <div className={styles['contact-info']}>
-                <p>{b.phone}</p>
+                <p>{formatPhoneNumber(phone)}</p>
                 {addressLines}
             </div>
             
